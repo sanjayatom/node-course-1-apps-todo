05ffb290f4a65aecdbc5c57fb70b03c537ae85f9
sanjay-PC\sanjay <vitustech2015@gmail.com>
2018-08-21 15:10:31 -0400

    test delete todo by id routes

--- server/server.js
+++ server/server.js
@@ -51,17 +51,17 @@ app.delete('/todos/:id',(req, res) => {
     // get the id
     let id = req.params.id;
     if(!ObjectID.isValid(id)){
-        return res.status(404).send('');
+        return res.status(404).send();
     }
 
     Todo.findByIdAndRemove(id).then((todo) => {
         if(!todo){
-            res.status(404).send('');
+            res.status(404).send();
         }
 
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
-        res.status(400).send('');
+        res.status(400).send();
     });
 });
 
