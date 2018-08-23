bdefcbc7faa5d72fae042723a6fa1d7bf8f29724
sanjay-PC\sanjay <vitustech2015@gmail.com>
2018-08-23 17:06:36 -0400

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require('crypto');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-}
+var password = 'abc123!';
 
-let token = jwt.sign(data, 'abc123');
+// bcrypt.genSalt(10, (err, salt) => {
+//     console.log(salt);
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-console.log(token);
+var hashedPassword = '$2a$10$q1lYZ/hxN/8ycUoSR0xU1eDidju3KlifnUqwbgn2S7ufspFLvHQa2';
 
-let decoded = jwt.verify(token, 'abc123');
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
 
-console.log('decoded ', decoded);
+// let data = {
+//     id: 10
+// }
+
+// let token = jwt.sign(data, 'abc123');
+
+// console.log(token);
+
+// let decoded = jwt.verify(token, 'abc123');
+
+// console.log('decoded ', decoded);
