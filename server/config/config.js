d4df0999fab675f0e0b0cfc558830c3b33801e5f
sanjay-PC\sanjay <vitustech2015@gmail.com>
2018-08-24 16:33:14 -0400

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 let env = process.env.NODE_ENV || 'development';
-console.log('env ***', env);
+// console.log('env ***', env);
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/db_Todo';
-}else if (env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/db_TodoTest';
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
