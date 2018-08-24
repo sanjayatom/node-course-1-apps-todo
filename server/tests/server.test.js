053757c1c5052fc477413d3e6fc9d1800df059eb
sanjay-PC\sanjay <vitustech2015@gmail.com>
2018-08-24 17:55:48 -0400

    made change for expect v 23

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -361,7 +361,7 @@ describe('POST /users/login', () => {
                 }
 
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens[1]).toMatchObject({
+                    expect(user.toObject().tokens[1]).toMatchObject({
                         access: 'auth',
                         token: res.headers['x-auth']
                     });
